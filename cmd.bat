54c25bba9206b17359db25a683943b204065fae0
marsnut <xwq20080101@gmail.com>
2019-07-18 10:22:59 +0800

    add plugin install file

---
+++ cmd.bat
@@ -0,0 +1,9 @@
+cnpm install vue-router
+:cnpm install vuex --save
+:cnpm install babel-plugin-component -D
+:cnpm install babel-preset-env -D
+:cnpm i element-ui -S
+:cnpm install vuex --save
+:cnpm install babel-plugin-component -D
+:cnpm install babel-preset-env -D
+:cnpm i element-ui -S
